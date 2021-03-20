4521395bcb58112fa087d4244cee0ba3a9cf9811
Uallesson Nunes Ivo <uallessons@gmail.com>
2021-03-20 07:23:48 -0300

    Funcionalidade de agendamento e contato

---
+++ assets/js/form.js
@@ -0,0 +1,20 @@
+var form = document.getElementById("my-form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("my-form-status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        status.innerHTML = "Em breve retornaremos confirmando o agendamento!";
+        form.reset()
+      }).catch(error => {
+        status.innerHTML = "Oops! Captamos algum erro durante a solicitação!"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
