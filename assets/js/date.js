73d21738ca88485f825078166e9c7e5977531480
Uallesson Nunes Ivo <uallessons@gmail.com>
2021-03-22 12:37:42 -0300

    Testes

---
+++ assets/js/date.js
@@ -0,0 +1,8 @@
+$('#sandbox-container .input-group.date').datepicker({
+    format: "dd/mm/yyyy",
+    startDate: "22/03/2021",
+    endDate: "26/03/2021",
+    todayBtn: true,
+    todayHighlight: true,
+    datesDisabled: ['03/06/2021', '03/21/2021']
+});
